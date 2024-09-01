f7b63796a882f66ef98028f2a4ed04945c5e3bfc
swtreacy <swtreacy@gmail.com>
2024-09-01 18:35:47 +0000

    Finished Lab 1

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,21 +1,23 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name: Sean Treacy
+#Date: September 1st, 2024
+#Assignment: First Program
 
 def main():
   print("First Program")
   #Say hello
-  
+  print("Hello, nice to meet you!")
   #Ask for the user's name
-
+  name = input("Please state your name.")
   #Use the user's name in the program.
-
+  print("A pleasure to be acquainted with you,", name)
   #Ask the user for their age.
-
+  age = input("How old are you?")
+  age = int(age)
   #Tell the user what year they were born in.
+  born = 2024 - age
   #Assume that they have not had their birthday yet this year.
-
+  print("You were born in", born)
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
--- MadLib.py
+++ MadLib.py
@@ -1,14 +1,19 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Sean Treacy
+#Date: September 1st, 2024
+#Assignment: MadLib
 
 def main():
   print("Madlib")
   #Ask user for words
-
+  person = input("Please write a person")
+  place = input("Please write a place")
+  object = input("Please write an object")
+  adjective = input("Please write an adjective")
+  number = input("Please write a number")
+  emotion = input("Please write an emotion")
   #Print the story with the user supplied words.
-
+  print("I went to" , place , "with" , person , "to pick up" , number , adjective , object , ". This made me" , emotion + "!" )
 
 
 #Call the main function if this is the file being run.
